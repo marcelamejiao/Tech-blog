6bc40207a3ffb534364a6cbb0c49fc3cceb98a3c
Marcela Mejia <mejiao24@hotmail.com>
2022-09-04 21:55:46 +1000

    Implement MVC paradigm

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'user',
+				key: 'id',
+				unique: false,
+			},
+		},
+		user_comment: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'post',
+				key: 'id',
+				unique: false,
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'comment',
+	}
+);
+
+module.exports = Comment;

5e7d4d746d4a48425f0bdd7dd80196a477303d2d
Marcela Mejia <mejiao24@hotmail.com>
2022-09-05 16:31:56 +1000

    Implement idle user logic

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,25 @@
-const logout = async () => {
+const logout = async (shouldRedirect) => {
+  if (shouldRedirect === undefined) {
+    shouldRedirect = true;
+  }
+
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    if (shouldRedirect) {
+      document.location.replace('/');
+    }
   } else {
     alert(response.statusText);
   }
 };
 
 document.querySelector('#logout').addEventListener('click', logout);
+
+// Log the user out if idle for 15 minutes
+setTimeout(() => {
+  logout(false);
+}, 15 * 60 * 1000);

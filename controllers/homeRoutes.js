c39277047cfc04d51470c65fc38ab11d333486e1
Marcela Mejia <mejiao24@hotmail.com>
2022-09-05 15:53:20 +1000

    Remove authentication middleware from homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // Get all posts and JOIN with user data
     const postsData = await Post.findAll({

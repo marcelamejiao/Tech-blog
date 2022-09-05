6c5f88a7cf37572432963444665882988d25f95e
Marcela Mejia <mejiao24@hotmail.com>
2022-09-05 13:06:39 +1000

    Implement add new comment

---
+++ public/js/newComment.js
@@ -0,0 +1,26 @@
+const sendComment = async (event) => {
+	event.preventDefault();
+
+    const post = document.querySelector(".post")
+    const postId = post.getAttribute("data-post");
+    const userComment = document.querySelector("#new-comment").value;
+
+    const response = await fetch(`/api/comments/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            post_id: postId,
+            user_comment: userComment
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(
+            'Failed to send a comment.'
+        );
+    }
+};
+
+document.querySelector("#send").addEventListener('click', sendComment);
